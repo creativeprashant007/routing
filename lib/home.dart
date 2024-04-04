52296c8ffcdacd68f28b15aaaf4d68028389df2a
creativeprashant007 <prashanttimalsina18@gmail.com>
2024-04-04 17:41:35 +0100

    state management

--- lib/home.dart
+++ lib/home.dart
@@ -12,19 +12,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _toggle = false;
+
+  void showToggle() {
+    setState(() {
+      _toggle = !_toggle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text("Flutter Routing"),
+          title: const Text("Flutter State Management"),
         ),
-        body: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, const SecondPage().route);
-              },
-              child: const Text("Press me")),
+        body: SizedBox(
+          width: double.infinity,
+          height: 700,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Visibility(
+                  visible: _toggle,
+                  child: const Text(
+                    "Widget is visible",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
+                  )),
+              ElevatedButton(
+                  onPressed: () {
+                    showToggle();
+                  },
+                  child: Text(!_toggle ? "Show me" : "Hide me")),
+            ],
+          ),
         ));
   }
 }
